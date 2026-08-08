1af09affe423d892f9068bfd2bbc8c7c2f0b209e
tuii05 <turui0532@gmail.com>
2026-08-08 10:44:03 +0200

    Removed

--- _/src/Order.cpp
+++
@@ -1,9 +0,0 @@
-#include "Order.h"
-#include <iostream>
-
-
-// Prints the necessary informations regarding the order.
-template <typename QuantityType, typename IDType, typename PriceType>
-void Order<QuantityType, IDType, PriceType>::print() const {
-    std::cout << quantity << "@ $" << price << std::endl;
-}
